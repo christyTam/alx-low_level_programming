779fb6d65b1269fc81cbf824ff714fc6771a17e5
christyTam <christyjames938@yahoo.com>
2024-01-02 19:55:12 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char character);
+
+void reset_to_98(int *num);
+
+void swap_int(int *num1, int *num2);
+
+int _strlen(char *val);
+
+void _puts(char *val);
+
+void print_rev(char *val);
+
+void rev_string(char *val);
+
+void puts2(char *val);
+
+void puts_half(char *val);
+
+void print_array(int *val, int num);
+
+char *_strcpy(char *destination, char *source);
+
+int _atoi(char *val);
+
+#endif
+
